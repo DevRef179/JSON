7bc9bbba17d678b61320bfd4a6b14f5d1cf15990
DevRef <72349761+DevRef179@users.noreply.github.com>
2023-06-07 01:06:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/json/UserModelClass.kt
@@ -0,0 +1,12 @@
+package com.example.json
+
+class UserModelClass (
+    val id: Int,
+    val name: String,
+    val email: String,
+    val gender: String,
+    val weight: Double,
+    val height: Int,
+    val mobile: String,
+    val office: String
+)
