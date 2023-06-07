edc8030dac7bf91c11135b4115f0a752281384bb
DevRef <72349761+DevRef179@users.noreply.github.com>
2023-06-07 02:03:34 +0200

    Gson funktioniert top jetzt veränderung

--- app/src/main/java/com/example/json/UserAdapter.kt
+++ app/src/main/java/com/example/json/UserAdapter.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class UserAdapter (val context: Context, val items: ArrayList<UserModelClass>) :
+class UserAdapter (private val context: Context, private val items: ArrayList<UserModelClass>) :
     RecyclerView.Adapter<UserAdapter.ViewHolder>() {
 
     /**
@@ -38,7 +38,7 @@ class UserAdapter (val context: Context, val items: ArrayList<UserModelClass>) :
      */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        val item = items.get(position)
+        val item = items[position]
 
         holder.tvId.text = item.id.toString()
         holder.tvName.text = item.name
@@ -64,14 +64,14 @@ class UserAdapter (val context: Context, val items: ArrayList<UserModelClass>) :
      */
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         // Holds the TextView that will add each item to
-        val tvId = view.findViewById<TextView>(R.id.tv_id)
-        val tvName = view.findViewById<TextView>(R.id.tv_name)
-        val tvEmail = view.findViewById<TextView>(R.id.tv_email)
-        val tvGender =view.findViewById<TextView>(R.id.tv_gender)
-        val tvWeight = view.findViewById<TextView>(R.id.tv_weight)
-        val tvHeight = view.findViewById<TextView>(R.id.tv_height)
-        val tvMobileNumber = view.findViewById<TextView>(R.id.tv_mobile)
-        val tvOfficeNumber = view.findViewById<TextView>(R.id.tv_office_number)
+        val tvId = view.findViewById<TextView>(R.id.tv_id)!!
+        val tvName = view.findViewById<TextView>(R.id.tv_name)!!
+        val tvEmail = view.findViewById<TextView>(R.id.tv_email)!!
+        val tvGender = view.findViewById<TextView>(R.id.tv_gender)!!
+        val tvWeight = view.findViewById<TextView>(R.id.tv_weight)!!
+        val tvHeight = view.findViewById<TextView>(R.id.tv_height)!!
+        val tvMobileNumber = view.findViewById<TextView>(R.id.tv_mobile)!!
+        val tvOfficeNumber = view.findViewById<TextView>(R.id.tv_office_number)!!
 
     }
 }
